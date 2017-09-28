ce837fd9d3323c2b611ccb81edf1938f73a06a88
Elbert Fliek <elbert@crowdynews.com>
2017-09-29 01:29:23 +0200

    added doc.go containing small package description for go doc

---
+++ doc.go
@@ -0,0 +1,8 @@
+/*
+Package imgsim is a library allows you to compute a fast perceptual hashes of an image.
+These hashes can then be used to compare images for similarity.
+Similar looking images will get similar perceptual hashes, unlike cryptographic hashes that
+would be very different for images with slight differences.
+This makes them suitable to measure how similar images are.
+*/
+package imgsim
